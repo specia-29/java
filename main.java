63fba2a620d9a2b78e17e4b9256553e0a22aaef5
EC2 Default User <specialized.19991129@gmail.com>
2023-04-04 12:44:46 +0900

    [Progate]変数の更新

--- main.java
+++ main.java
@@ -1,18 +1,21 @@
 class Main {
   public static void main(String[] args) {
-    int number1 = 3;
+    int number = 11;
+    String text = "Ruby";
+    System.out.println(number);
+    System.out.println(text);
 
-    // int型の変数number2を定義し、7を代入してください
-    int number2 = 7;
+    // 変数numberを9で上書きしてください
+    number = 9;
 
-    // number1 * number2を出力してください
-    System.out.println(number1 * number2);
+    // 変数numberを出力してください
+    System.out.println(number);
 
-    // 変数textに「プログラミングを勉強しよう」を代入してください
-    String text = "プログラミングを勉強しよう";
+    // 変数textを「Java」で上書きしてください
+    text = "Java";
 
-    // 「Progateで」と変数textを連結して出力してください
-    System.out.println("Progateで" + text);
+    // 変数textを出力してください
+    System.out.println(text);
 
   }
 }
