48771d394760583ea5547cdf60f84bc6440b8907
EC2 Default User <specialized.19991129@gmail.com>
2023-04-05 13:32:59 +0900

    [Progate]課題演習BMIを求める

--- main.java
+++ main.java
@@ -1,21 +1,26 @@
 class Main {
   public static void main(String[] args) {
-    int length = 6;
-    int height = 8;
+    // 変数nameに、好きな名前を代入してください
+    String name = "大谷翔平";
 
-    // 変数rectangleAreaに、四角形の面積を代入してください
-    // int型で指定することを忘れないこと。
-    int rectangleArea = length * height;
+    // 変数ageに、好きな年齢を代入してください
+    int age = 27;
 
-    // 変数rectangleAreaを出力してください
-    System.out.println(rectangleArea);
+    // 変数heightに、好きな身長を代入してください（例: 1.2）
+    double height = 1.93;
 
-    // 変数triangleAreaに、三角形の面積を代入してください
-    // int型で指定することを忘れないこと。
-    int triangleArea = length * height / 2;
+    // 変数weightに、好きな体重を代入してください（例: 20.0）
+    double weight = 90.0;
 
-    // 変数triangleAreaを出力してください
-    System.out.println(triangleArea);
+    // 変数bmiに、BMIを計算して代入してください
+    double bmi = weight / height / height;
+
+    // 名前、年齢、身長、体重、BMIに関する情報を出力してください
+    System.out.println("名前は" + name + "です");
+    System.out.println("年齢は" + age + "歳です");
+    System.out.println("身長は" + height + "mです");
+    System.out.println("体重は" + weight + "kgです");
+    System.out.println("BMIは" + bmi + "です");
 
   }
 }
