4bb4dcdf7190ec33fcc94ba9da92c7a872132f1f
EC2 Default User <specialized.19991129@gmail.com>
2023-04-06 15:01:34 +0900

    [Progate]大小比較

--- main.java
+++ main.java
@@ -24,3 +24,15 @@ class Main {
 
   }
 }
+
+
+class Main {
+  public static void main(String[] args) {
+    // 「4 + 2」と「6」を比較し、falseとなるようにしてください
+    System.out.println(4 + 2 > 6);
+    
+    // 「4 + 2」と「6」を比較し、trueとなるようにしてください
+    System.out.println(4 + 2 >= 6);
+    
+  }
+}
